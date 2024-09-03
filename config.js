05c5126def0c46bb41d53b531ca5869ca32ace38
soniji03 <jbpomsoni03@gmail.com>
2024-09-03 16:38:44 +0530

    8th

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-// export const API_BACKEND_URL = 'https://whiztalk-back.onrender.com';
-export const API_BACKEND_URL = 'http://localhost:8080';
+export const API_BACKEND_URL = 'https://whiztalk-back.onrender.com';
+// export const API_BACKEND_URL = 'http://localhost:8080';
 
 console.log("API_BACKEND_URL:---",  API_BACKEND_URL)
